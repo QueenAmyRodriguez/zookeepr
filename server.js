a90cf2a53960e16e3b29b8edb8d8eaa6abe7f350
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-02-20 22:04:43 -0700

    separated routes and added tests

--- server.js
+++ server.js
@@ -1,71 +1,22 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
-const { animals } = require("./data/animals");
+const { animals } = require("./data/animals.json");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+// instructs server to make files avail
+app.use(express.static("public"));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-// instructs server to make files avail
-app.use(express.static("public"));
 
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-
-app.get("/api/animals", (req, res) => {
-  // asigned animal array to results
-  let results = animals;
-  if (req.query) {
-    //   calling the function and giving it 2 parameters and setting it = to results
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
-});
-
-app.get("/api/animals/:id", (req, res) => {
-  const result = findById(req.params.id, animals);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
-
-// getting index to be served from our express.js server using / which is the root route of the server
-// / is used to create the homepage of a server
-app.get("/", (req, res) => {
-  // uses res.sendfile to send file we want server to read back to client
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.get("/animals", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/animals.html"));
-});
-
-app.get("/zookeepers", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
-
-// route on server that accepts data to be used or stored server-side
-app.post("/api/animals", (req, res) => {
-  // set id based on what the next index of the array will be
-  req.body.id = animals.length.toString();
-
-  // if any data in req.body is incorrect, send 400 error back
-  if (!validateAnimal(req.body)) {
-    res.status(400).send("The animal is not properly formatted.");
-  } else {
-    const animal = createNewAnimal(req.body, animals);
-    res.json(animal);
-  }
-});
 app.listen(PORT, () => {
   console.log(`API Server now on port ${PORT}!`);
 });
