8458a86110dcc525eefe8ee1498db8dc9ccfae69
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-02-20 22:26:16 -0700

    added zookeepers

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const animalRoutes = require("../apiRoutes/apiRoutes");
 
+router.use(require('./zookeeperRoutes'));
 router.use(animalRoutes);
 
 module.exports = router;
